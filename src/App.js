2fe9de560eca15414f7559290170191f418802a7
Somaya <somaya1123@gmail.com>
2022-06-12 01:28:00 -0400

    updated post img and header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import { Component } from 'react';
+import MainHeader from './components/MainHeader'
+import Bar from './components/Bar/Bar'
+import AboutUs from './components/AboutUs/AboutUs';
 import './App.css';
+import Post from './components/Posts/Post';
 
 export default class App extends Component {
   render() {
     return (
-      <main className="App">
-        Blog
-      </main>
+      <div className="App">
+        <MainHeader/>
+        <Post/>
+        {/* <Bar/> */}
+        {/* <AboutUs/> */}
+        
+      </div>
     )
   }
 }
