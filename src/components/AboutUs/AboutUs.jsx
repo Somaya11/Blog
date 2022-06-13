837596fb2ffcd2c5b43538e484f9681b1dbf43b1
Somaya <somaya1123@gmail.com>
2022-06-12 22:13:42 -0400

    adding aboutus

--- src/components/AboutUs/AboutUs.jsx
+++ src/components/AboutUs/AboutUs.jsx
@@ -1,7 +1,59 @@
-import React from 'react'
+import "./AboutUs.css"
 
 export default function AboutUs() {
   return (
-    <div>AboutUs</div>
+    <body className="aboutusBody">
+      
+      <div className="paragraph">
+      <h1 >About Us</h1>
+
+        <h3 className> Lorem ipsum dolor sit amet </h3>
+          <article>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dicta consectetur aperiam voluptate accusantium totam labore voluptates dolore soluta laborum voluptatem explicabo pariatur corporis, esse, sint ratione amet nostrum et neque. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellendus aliquam officia mollitia? Pariatur nobis alias quidem, consectetur porro minima sapiente, earum quibusdam neque consequuntur tempora illo, optio eum id beatae!
+          </article>
+        
+          <h3 > Lorem ipsum dolor sit amet </h3>
+
+          <article>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fuga alias id quia ipsa ut.Lorem ipsum dolor sit amet consectetur adipisicing elit. Non deleniti assumenda eligendi at iure. Beatae a, nisi laboriosam maiores harum provident veniam omnis accusantium quae illo nulla numquam id tempora. 
+          </article>
+          
+        <img
+        className="Rectangle7"
+        src="https://i.pinimg.com/originals/b6/88/45/b68845a212688ba060337267ba240746.jpg"
+        alt=""
+        />
+
+        <img className="Rectangle5"
+        src="https://assets.bigcartel.com/product_images/166395214/Girl_Boss.jpg?auto=format&fit=max&h=1000&w=1000"
+        alt=""
+        />
+        
+        
+
+        </div>
+
+        <div className="paragraph2">
+        
+        <article>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fuga alias id quia ipsa ut.Lorem ipsum dolor sit amet consectetur adipisicing elit. Non deleniti assumenda eligendi at iure. Beatae a, nisi laboriosam maiores harum provident veniam omnis accusantium quae illo nulla numquam id tempora. 
+        <br>
+        </br>
+        <br>
+        </br>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fuga alias id quia ipsa ut.Lorem ipsum dolor sit amet consectetur adipisicing elit. Non deleniti assumenda eligendi at iure. Beatae a, nisi laboriosam maiores harum provident veniam omnis accusantium quae illo nulla numquam id tempora.
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fuga alias id quia ipsa ut.Lorem ipsum dolor sit amet consectetur adipisicing elit. Non deleniti assumenda eligendi at iure. 
+        <br>
+        </br>
+        <br>
+        </br>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fuga alias id quia ipsa ut.Lorem ipsum dolor sit amet consectetur adipisicing elit. Non deleniti assumenda eligendi at iure. Beatae a, nisi laboriosam maiores harum provident veniam omnis accusantium quae illo nulla numquam id tempora.
+        </article>
+
+        </div>
+
+        
+      
+      
+    </body>
+    
   )
 }
